cf83b40233df77de502338816240b9939390baab
JoshuaOLoduca <81768747+JoshuaOLoduca@users.noreply.github.com>
2022-05-29 13:25:45 -0400

    styled and layout of pong

--- commands/pong.js
+++ commands/pong.js
@@ -4,8 +4,18 @@ class Pong {
   render(element, cleanupCb) {
     $(element).html(
       `
-      <div>hi bitch</div>
-      <button id="pong_remove">exit</button>
+      <div id='pong__container'>
+        <div id='pong__table'>
+          <div id='pong__scoreboard'>
+            <div id='pong__scoreboard__left'>0</div>
+            <div id='pong__scoreboard__right'>0</div>
+          </div>
+          <div class='pong__paddle' id='pong__left'> </div>
+          <div id='pong__ball'> </div>
+          <div class='pong__paddle' id='pong__right'> </div>
+        </div>
+        <button id="pong_remove">exit</button>
+      </div>
       `
     );
 
